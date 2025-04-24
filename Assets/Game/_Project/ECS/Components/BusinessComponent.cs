c7ae811fff42df78ca0e0c8d1b3f0a65d41e8f2c
N-u-j-e-e <310895@mail.ru>
2025-04-24 23:59:40 +0300

    Add: minor changes

---
+++ Assets/Game/_Project/ECS/Components/BusinessComponent.cs
@@ -0,0 +1,6 @@
+public struct BusinessComponent
+{
+    public float IncomeProgress;
+    public int Level;
+    public int UpgradesPurchased;
+}
---
+++ Assets/Game/_Project/Entry/EntryInstaller.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using Leopotam.EcsLite;
+using UnityEngine.SceneManagement;
+
+public class EntryInstaller : MonoBehaviour
+{
+    [field: SerializeField] private GameConfig gameConfig;
+    [field: SerializeField] private SaveService saveService;
+
+    private EcsWorld _world;
+    private EcsSystems _systems;
+
+    private void Awake()
+    {
+        _world = new EcsWorld();
+        _systems = new EcsSystems(_world);
+
+        var (playerBalance, businessData) = saveService.LoadProgress(gameConfig.businesses.Length);
+
+        InitBusinessData(businessData);
+
+        SceneManager.LoadScene("MainScene");
+    }
+
+    private void InitBusinessData(BusinessData[] businessData)
+    {
+        var businessComponentPool = _world.GetPool<BusinessComponent>();
+
+        for (int i = 0; i < businessData.Length; i++)
+        {
+            ref var business = ref businessComponentPool.Add(_world.NewEntity());
+            business.Level = businessData[i].Level;
+            business.IncomeProgress = businessData[i].IncomeProgress;
+            business.UpgradesPurchased = businessData[i].UpgradesPurchased;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+       //saveService.SaveProgress()
+    }
+}

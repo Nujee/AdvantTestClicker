c86dfdcb1842a305ed50f2d8dabd6fc50a345e34
N-u-j-e-e <310895@mail.ru>
2025-04-30 14:41:11 +0300

    Upd: update config assets

--- Assets/Game/_Project/Configs/TitlesConfigs/TitlesConfig.cs
+++ Assets/Game/_Project/Configs/TitlesConfigs/TitlesConfig.cs
@@ -4,6 +4,5 @@ using UnityEngine;
 public sealed class TitlesConfig : ScriptableObject
 {
     [field: SerializeField] public string BusinessTitle { get; private set; }
-    [field: SerializeField] public string Upgrade1Title { get; private set; }
-    [field: SerializeField] public string Upgrade2Title { get; private set; }
+    [field: SerializeField] public IdMapSerializable<string> UpgradeTitlesById { get; private set; }
 }

8e9105754945221890c5025a10146f76ddaae4df
N-u-j-e-e <310895@mail.ru>
2025-05-01 03:53:06 +0300

    Add: add fully working SaveService; Mod: EntryPoint now uses load/save from SaveService

--- Assets/Game/_Project/Core/EntryPoint.cs
+++ Assets/Game/_Project/Core/EntryPoint.cs
@@ -1,34 +1,149 @@
 using UnityEngine;
 using Leopotam.EcsLite;
-using static Leopotam.EcsLite.EcsWorld;
-using static UnityEngine.EventSystems.EventTrigger;
+using UnityEngine.UI;
 
 public sealed class EntryPoint : MonoBehaviour
 {
     [field: SerializeField] public BalancePanelView BalancePanelView { get; private set; }
     [field: SerializeField] public RectTransform BusinessViewsParent { get; private set; }
     [field: SerializeField] public BusinessPanelView BusinessPanelPrefab { get; private set; }
+    [field: SerializeField] public ScrollRect Scroll { get; private set; }
 
     [field: SerializeField] public GameSettingsConfig Settings { get; private set; }
-    [field: SerializeField] public SaveService SaveService { get; private set; }
 
     private EcsWorld _world;
     private IEcsSystems _systems;
+    private SaveService _saveService;
 
     private void Start()
     {
         _world = new EcsWorld();
         _systems = new EcsSystems(_world);
+        _saveService = new SaveService(Settings, _world);
 
-        //var (playerBalance, businessStates) = SaveService.LoadProgress(Settings.Businesses.Length);
+        SaveData saveData = _saveService.LoadProgress();
 
         var playerEntity = _world.NewEntity();
 
-        InitPlayer(playerEntity);
-
-        InitBusinesses(playerEntity);
-
+        InitPlayer();
+        InitBusinesses();
+        SetupPlayer();
         InitSystems();
+
+        ResetScrollToTop();
+
+        void InitPlayer()
+        {
+            ref var c_playerBalance = ref _world.GetPool<c_Balance>().Add(playerEntity);
+            BalancePanelView.Init(_world, playerEntity);
+            c_playerBalance.Amount.Value = saveData.PlayerBalance;
+        }
+
+        void InitBusinesses()
+        {
+            var businesses = Settings.BusinessConfigsById.Map;
+            var businessViewPool =
+                new GenericPool<BusinessPanelView>(BusinessPanelPrefab, businesses.Count, BusinessViewsParent);
+
+            foreach (var pair in businesses)
+            {
+                var businessView = businessViewPool.Get();
+
+                var businessId = pair.Key;
+                var businessConfig = pair.Value;
+
+                var businessSaveData = saveData.BusinessesById[businessId];
+
+                var businessEntity = _world.NewEntity();
+
+                // State initialization is split to let BusinessView subscribe to reactive properties
+                // before initial values are set, ensuring proper visual updates.
+                InitData();
+                InitState();
+                InitView();
+                InitUpgrades();
+                SetStateValues();
+
+                void InitData()
+                {
+                    ref var c_data = ref _world.GetPool<c_BusinessData>().Add(businessEntity);
+                    c_data.Id = businessId;
+                    c_data.PlayerEntityPacked = _world.PackEntity(playerEntity);
+                }
+
+                void InitState()
+                {
+                    ref var c_state = ref _world.GetPool<c_BusinessState>().Add(businessEntity);
+                }
+
+                void InitView()
+                {
+                    businessView.Init(Settings, _world, businessEntity, playerEntity);
+                }
+
+                void InitUpgrades()
+                {
+                    ref var c_data = ref _world.GetPool<c_BusinessData>().Get(businessEntity);
+                    ref var c_upgrades = ref _world.GetPool<c_BusinessUpgrades>().Add(businessEntity);
+
+                    foreach (var pair in pair.Value.Upgrades)
+                    {
+                        var upgradeId = pair.Key;
+                        var upgradeConfig = pair.Value;
+                        var upgradeInfo = c_upgrades.InfoById;
+
+                        upgradeInfo[upgradeId] = new UpgradeInfo
+                        (
+                            purchaseData: default,
+                            factor: upgradeConfig.PercentFactor * 0.01f
+                        );
+
+                        var upgradeView = businessView.UpgradeViews[upgradeId];
+                        upgradeView.Init(upgradeId, Settings, _world, businessEntity, playerEntity);
+
+                        var isUpgradePurchased = businessSaveData.UpgradeStatusesById[upgradeId];
+                        var upgradePurchaseData = upgradeInfo[upgradeId].PurchaseData;
+
+                        upgradePurchaseData.Value = (upgradeConfig.Price, isUpgradePurchased);
+                    }
+                }
+
+                void SetStateValues()
+                {
+                    ref var c_state = ref _world.GetPool<c_BusinessState>().Get(businessEntity);
+
+                    c_state.Level.Value = businessSaveData.Level;
+                    c_state.LevelUpPrice.Value = (c_state.Level.Value + 1) * businessConfig.BasePrice;
+                    c_state.Income.Value = businessSaveData.Income;
+
+                    var rawDelay = businessSaveData.IncomeDelay;
+                    var normalizedDelay = rawDelay / businessConfig.BaseIncomeDelay;
+                    c_state.IncomeDelayElapsed.Value = (rawDelay, normalizedDelay);
+
+                    if (c_state.Level.Value >= 1)
+                        _world.GetPool<t_IsPurchased>().Add(businessEntity);
+                }
+            }
+        }
+
+        void SetupPlayer()
+        {
+            ref var c_playerBalance = ref _world.GetPool<c_Balance>().Get(playerEntity);
+            BalancePanelView.Init(_world, playerEntity);
+            c_playerBalance.Amount.Value = saveData.PlayerBalance;
+        }
+
+        void InitSystems()
+        {
+            _systems
+                .Add(new s_UpdateBalance())
+                .Add(new s_UpdateBusinessIncome(Settings))
+                .Add(new s_IncreaseBusinessLevel(Settings))
+                .Add(new s_BuyBusinessUpgrade())
+                .Add(new s_ElapseIncomeDelay(Settings))
+                .Add(new s_CollectBusinessIncome(Settings))
+                .Init();
+        }
     }
 
     private void Update()
@@ -44,99 +159,11 @@ public sealed class EntryPoint : MonoBehaviour
 
     private void OnApplicationQuit()
     {
-        //saveService.SaveProgress();
+        _saveService.SaveProgress();
     }
 
-    private void InitPlayer(int playerEntity)
+    private void ResetScrollToTop()
     {
-        ref var c_playerBalance = ref _world.GetPool<c_Balance>().Add(playerEntity);
-        BalancePanelView.Init(_world, playerEntity);
-        c_playerBalance.Amount.Value = Settings.InitialBalance;
-    }
-
-    private void InitBusinesses(int playerEntity)
-    {
-        var businessViewPool = new GenericPool<BusinessPanelView>(BusinessPanelPrefab, 5, BusinessViewsParent);
-
-        foreach (var businessConfigById in Settings.BusinessConfigsById)
-        {
-            var businessEntity = _world.NewEntity();
-            var businessId = businessConfigById.Key;
-            var businessConfig = businessConfigById.Value;
-
-            // State initialization is split to let BusinessView subscribe to reactive properties
-            // before initial values are set, ensuring proper visual updates.
-            InitData();
-            InitState();
-            InitUpgrades(InitView());
-            SetStateValues();
-
-            void InitData()
-            {
-                ref var c_data = ref _world.GetPool<c_BusinessData>().Add(businessEntity);
-                c_data.Id = businessConfigById.Key;
-                c_data.PlayerEntityPacked = _world.PackEntity(playerEntity);
-            }
-
-            void InitState()
-            {
-                ref var c_state = ref _world.GetPool<c_BusinessState>().Add(businessEntity);
-            }
-
-            BusinessPanelView InitView()
-            {
-                var view = businessViewPool.Get();
-                view.Init(Settings, _world, businessEntity);
-
-                return view;
-            }
-
-            void SetStateValues()
-            {
-                ref var c_state = ref _world.GetPool<c_BusinessState>().Get(businessEntity);
-                c_state.Level.Value = (businessId == 1) ? 1 : 0;
-                c_state.LevelUpPrice.Value = (c_state.Level.Value + 1) * businessConfig.BasePrice;
-                c_state.Income.Value = businessConfig.BaseIncome;
-                c_state.IncomeDelayElapsed.Value = (0f, 0f);
-
-                if (c_state.Level.Value == 1)
-                    _world.GetPool<t_IsPurchased>().Add(businessEntity);
-            }
-
-            void InitUpgrades(BusinessPanelView view)
-            {
-                ref var c_upgrades = ref _world.GetPool<c_BusinessUpgrades>().Add(businessEntity);
-                foreach (var upgradeConfigById in businessConfigById.Value.Upgrades)
-                {
-                    var upgradeId = upgradeConfigById.Key;
-                    var upgradeConfig = upgradeConfigById.Value;
-                    var upgradeInfo = c_upgrades.InfoById;
-
-                    upgradeInfo[upgradeId] = new UpgradeInfo
-                    (
-                        purchaseData: default,
-                        factor: upgradeConfig.PercentFactor * 0.01f
-                    );
-
-                    var upgradeView = view.UpgradeViews[upgradeId];
-                    upgradeView.Init(upgradeId, Settings, _world, businessEntity);
-
-                    var upgradePurchaseData = upgradeInfo[upgradeId].PurchaseData;
-                    upgradePurchaseData.Value = (upgradeConfig.Price, false);
-                }
-            }
-        }
-    }
-
-    private void InitSystems()
-    {
-        _systems
-            .Add(new s_UpdateBalance())
-            .Add(new s_UpdateBusinessIncome(Settings))
-            .Add(new s_IncreaseBusinessLevel(Settings))
-            .Add(new s_BuyBusinessUpgrade())
-            .Add(new s_ElapseIncomeDelay(Settings))
-            .Add(new s_CollectBusinessIncome(Settings))
-            .Init();
+        Scroll.normalizedPosition = new Vector2(Scroll.normalizedPosition.x, 1f);
     }
 }
--- Assets/Game/_Project/Core/SaveService.cs
+++ Assets/Game/_Project/Core/SaveService.cs
@@ -1,47 +1,207 @@
+using Leopotam.EcsLite;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class SaveService
+public sealed class SaveService
 {
-    private const string PlayerBalanceKey = "PlayerBalance";
-    private const string BusinessCurrentIncomeDelayKey = "BusinessCurrentIncomeDelay";
-    private const string BusinessLevelKey = "BusinessLevel";
-    private const string BusinessUpgradesKey = "BusinessUpgrades";
+    private GameSettingsConfig _settings;
+    private EcsWorld _world;
 
-    private readonly GameSettingsConfig _settings;
+    private string SavePath => Path.Combine(Application.persistentDataPath, "game_save.json");
 
-    public SaveService(GameSettingsConfig settings) => _settings = settings;
-
-    public (float playerBalance, BusinessState[] businessData) LoadProgress(int numberOfBusinesses)
+    public SaveService(GameSettingsConfig settings, EcsWorld world)
     {
-        var playerBalance = PlayerPrefs.GetFloat(PlayerBalanceKey, _settings.InitialBalance);
+        _settings = settings;
+        _world = world;
+    }
 
-        var businessData = new BusinessState[numberOfBusinesses];
-        for (int i = 0; i < numberOfBusinesses; i++)
+    public SaveData LoadProgress()
+    {
+        if (!File.Exists(SavePath))
+            return CreateDefaultSave();
+
+        var jsonData = new JsonSaveData();
+        try
         {
-            //var incomeProgress = PlayerPrefs.GetFloat($"{BusinessIncomeProgressKey}_{i}", 0f);
-            var defaultLevel = (i == 0) ? 1 : 0; 
-            var level = PlayerPrefs.GetInt($"{BusinessLevelKey}_{i}", defaultLevel);
-            var upgradesPurchased = PlayerPrefs.GetInt($"{BusinessUpgradesKey}_{i}", 0);
-
-            //businessData[i] = new BusinessData(incomeProgress, level, upgradesPurchased);
+            string json = File.ReadAllText(SavePath);
+            jsonData = JsonUtility.FromJson<JsonSaveData>(json);
+            return ConvertFromJson();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Load failed: {e}");
+            return CreateDefaultSave();
         }
 
-        return (playerBalance, businessData);
+        SaveData CreateDefaultSave()
+        {
+            var defaultBalance = _settings.InitialBalance;
+            var businessesById = new Dictionary<int, BusinessData>();
+
+            foreach (var pair in _settings.BusinessConfigsById)
+            {
+                var businessId = pair.Key;
+                var config = pair.Value;
+
+                var upgradeStatuses = new Dictionary<int, bool>();
+
+                foreach (var upgradePair in config.Upgrades)
+                    upgradeStatuses[upgradePair.Key] = false;
+
+                // If dict is empty, then newly created BusinessData is the 1st one,
+                // so its level is set to 1, and 0 for all of the rest.
+                businessesById[businessId] = new BusinessData(
+                    level: (businessesById.Count == 0) ? 1 : 0,
+                    income: config.BaseIncome,
+                    delay: 0f,
+                    upgradeStatusesById: upgradeStatuses
+                );
+            }
+
+            return new SaveData(defaultBalance, businessesById);
+        }
+
+        SaveData ConvertFromJson()
+        {
+            var businesses = new Dictionary<int, BusinessData>();
+            foreach(var jsonBusiness in jsonData.Businesses)
+            {
+                var upgradeStatuses = new Dictionary<int, bool>();
+                foreach (var jsonUpgrade in jsonBusiness.Upgrades)
+                {
+                    upgradeStatuses[jsonUpgrade.Id] = jsonUpgrade.IsPurchased;
+                }
+
+                businesses[jsonBusiness.Id] = new BusinessData(
+                    level: jsonBusiness.Level,
+                    income: jsonBusiness.Income,
+                    delay: jsonBusiness.IncomeDelay,
+                    upgradeStatusesById: upgradeStatuses
+                );
+            }
+
+            return new SaveData(jsonData.PlayerBalance, businesses);
+        }
+    }
+
+    public void SaveProgress()
+    {
+        var jsonData = new JsonSaveData();
+        try
+        {
+            jsonData = ConvertToJson();
+            var json = JsonUtility.ToJson(jsonData, prettyPrint: true);
+            File.WriteAllText(SavePath, json);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Save failed: {e}");
+        }
+
+        JsonSaveData ConvertToJson()
+        {
+            foreach (var entity in _world.Filter<c_Balance>().End())
+            {
+                jsonData.PlayerBalance = _world.GetPool<c_Balance>().Get(entity).Amount.Value;
+                break;
+            }
+
+            var businessEntities = _world
+                .Filter<c_BusinessData>()
+                .Inc<c_BusinessState>()
+                .Inc<c_BusinessUpgrades>()
+                .End();
+
+            jsonData.Businesses = new JsonBusinessData[businessEntities.GetEntitiesCount()];
+
+            var dataPool = _world.GetPool<c_BusinessData>();
+            var statePool = _world.GetPool<c_BusinessState>();
+            var upgradesPool = _world.GetPool<c_BusinessUpgrades>();
+
+            var businessIndex = 0;
+            foreach (var entity in businessEntities)
+            {
+                ref var data = ref dataPool.Get(entity);
+                ref var state = ref statePool.Get(entity);
+                ref var upgrades = ref upgradesPool.Get(entity);
+
+                var upgradesData = new JsonUpgradeData[upgrades.InfoById.Count];
+
+                var upgradeIndex = 0;
+                foreach (var upgrade in upgrades.InfoById)
+                {
+                    upgradesData[upgradeIndex++] = new JsonUpgradeData
+                    {
+                        Id = upgrade.Key,
+                        IsPurchased = upgrade.Value.PurchaseData.Value.isPurchased
+                    };
+                }
+
+                jsonData.Businesses[businessIndex++] = new JsonBusinessData
+                {
+                    Id = data.Id,
+                    Level = state.Level.Value,
+                    Income = state.Income.Value,
+                    IncomeDelay = state.IncomeDelayElapsed.Value.raw,
+                    Upgrades = upgradesData
+                };
+            }
+
+            return jsonData;
+        }
+    }
+
+    [Serializable]
+    private class JsonSaveData
+    {
+        public float PlayerBalance;
+        public JsonBusinessData[] Businesses;
+    }
+
+    [Serializable]
+    private class JsonBusinessData
+    {
+        public int Id;
+        public int Level;
+        public float Income;
+        public float IncomeDelay;
+        public JsonUpgradeData[] Upgrades;
+    }
+
+    [Serializable]
+    private class JsonUpgradeData
+    {
+        public int Id;
+        public bool IsPurchased;
     }
 }
 
-[Serializable]
-public struct BusinessState
+public readonly struct SaveData
 {
-    public int Level;
-    public float IncomeProgress;
-    public bool[] UpgradesPurchased;
+    public readonly float PlayerBalance;
+    public readonly Dictionary<int, BusinessData> BusinessesById;
 
-    public BusinessState(int level, float incomeProgress, bool[] upgradesPurchased)
+    public SaveData(float balance, Dictionary<int, BusinessData> businessesById)
+    {
+        PlayerBalance = balance;
+        BusinessesById = businessesById;
+    }
+}
+
+public readonly struct BusinessData
+{
+    public readonly int Level;
+    public readonly float Income;
+    public readonly float IncomeDelay;
+    public readonly Dictionary<int, bool> UpgradeStatusesById;
+
+    public BusinessData(int level, float income, float delay, Dictionary<int, bool> upgradeStatusesById)
     {
         Level = level;
-        IncomeProgress = incomeProgress;
-        UpgradesPurchased = upgradesPurchased;
+        Income = income;
+        IncomeDelay = delay;
+        UpgradeStatusesById = upgradeStatusesById;
     }
 }
